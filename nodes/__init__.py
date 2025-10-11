ec300ba7169e9454cc3ea70acb3b331076d57e40
Dylan Turnbull <dylanturn@gmail.com>
2025-10-10 23:18:43 -0500

    another crack configs

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -3,6 +3,7 @@ from .sdxl_lora_prompter import *
 from .generate_image import *
 from .lora_discovery import *
 from .endpoint_config import *
+from .text_storage import *
 
 NODE_CLASS_MAPPINGS = {
     "ComfyBros_OllamaConnection": OllamaConnection,
@@ -12,6 +13,9 @@ NODE_CLASS_MAPPINGS = {
     "ComfyBros_GenerateImage": GenerateImage,
     "ComfyBros_LoraDiscovery": LoraDiscovery,
     "ComfyBros_EndpointConfiguration": EndpointConfiguration,
+    "ComfyBros_TextStorage": TextStorage,
+    "ComfyBros_TextTemplate": TextTemplate,
+    "ComfyBros_TextConcatenate": TextConcatenate,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -22,4 +26,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ComfyBros_GenerateImage": "Generate Image",
     "ComfyBros_LoraDiscovery": "LORA Discovery",
     "ComfyBros_EndpointConfiguration": "Endpoint Configuration",
+    "ComfyBros_TextStorage": "Text Storage",
+    "ComfyBros_TextTemplate": "Text Template",
+    "ComfyBros_TextConcatenate": "Text Concatenate",
 }
