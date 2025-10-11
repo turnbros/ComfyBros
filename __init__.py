b63c099c2e8ef0306c770cd4ba9e89b0a622fc20
Dylan Turnbull <dylanturn@gmail.com>
2025-10-10 19:32:38 -0500

    Revert "added UI extension"
    This reverts commit 98a5d9ceebfaff77a392877be1e64d89446c465e.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,3 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-# Initialize web extension for settings interface
-try:
-    from . import web_extension
-except Exception as e:
-    print(f"ComfyBros: Warning - Could not initialize web extension: {e}")
-
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
-# Web directory for ComfyUI to serve static files
-WEB_DIRECTORY = "./web"
--- web/comfybros.js
+++
@@ -1,6 +0,0 @@
-/**
- * ComfyBros Main Web Extension Entry Point
- * This file is automatically loaded by ComfyUI
- */
-
-import "./comfybros_settings.js";
