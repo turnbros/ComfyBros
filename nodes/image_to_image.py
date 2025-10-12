a120661f51b0a4024899baa3d4121e5416e7a330
Dylan Turnbull <dylanturn@gmail.com>
2025-10-11 21:14:13 -0500

    troubleshoot

--- nodes/image_to_image.py
+++ nodes/image_to_image.py
@@ -225,6 +225,17 @@ class ImageToImage:
             response = requests.post(endpoint, headers=headers, json=payload)
             response.raise_for_status()
             result = response.json()
+
+            print(f"Full response from RunPod:{vars(result)}")
+
+            """
+              "delayTime": 707,
+  "id": "sync-853a97da-3d7f-4659-96b4-4443a603c21b-u1",
+  "status": "IN_PROGRESS",
+  "workerId": "ljaic2fgukybe3"
+}
+            """
+
         except requests.exceptions.RequestException as exc:
             raise RuntimeError(f"Request error: {exc}") from exc
         except json.JSONDecodeError as exc:
